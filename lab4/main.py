71cd32d2487937967061ecfa797855144dcd9051
Luiza Abdrashitova <luizik58@gmail.com>
2020-12-13 00:30:51 +0300

    lab4

--- lab4/main.py
+++ lab4/main.py
@@ -1,4 +1,4 @@
-from math import sqrt, exp, pi
+from math import sqrt, exp, floor, pi
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -44,4 +44,21 @@ for d in ds:
             ax1.set_ylabel(f'$f(x), n = {ns[i]}$')
             if i == 0:
                 ax1.set_title((d.name if j == 1 else '') + f"\n\n$h = h_n * {ks[j]}$")
+    plt.show()
+
+
+for d in ds:
+    fig, ax = plt.subplots(1, len(ns))
+    rng = (-4, 4) if not d.discrete() else (6, 14)
+    for i in range(len(ns)):
+        data = [d.x() for j in range(ns[i])]
+        x = np.linspace(min(data), max(data), 200)
+        yF = [d.F(k) for k in x]
+        plt.subplot(1, 3, i+1)
+        plt.title(d.name + ", n = " + str(ns[i]))
+        plt.xlabel('x')
+        plt.ylabel('Функция распределения')
+        plt.plot(x, yF, label='ожидаемое')
+        plt.hist(data, density=True, label='полученное', bins=floor(len(data)),histtype='step',cumulative=True)
+        plt.legend()
     plt.show()
